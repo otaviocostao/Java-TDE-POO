59aa4163b879ce85751c9bb8992610a35b3a1b10
Otavio Costa <dev.otaviocostao@gmail.com>
2024-09-07 13:17:39 -0300

    Todos os metodos da classe main para a classe GerenciadorTrabug

--- src/GerenciadorTrackBug.java
+++ src/GerenciadorTrackBug.java
@@ -1,26 +1,102 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class GerenciadorTrackBug {
-    ArrayList<Funcionario> listaFuncionarios = new ArrayList<>();
-    ArrayList<Equipamento> listaEquipamentos = new ArrayList<>();
-    ArrayList<Emprestimos> listaEmprestimentos = new ArrayList<>();
-
-    Scanner scanner = new Scanner(System.in);
+    static ArrayList<Funcionario> listaFuncionarios = new ArrayList<>();
+    static ArrayList<Equipamento> listaEquipamentos = new ArrayList<>();
+    static ArrayList<Emprestimos> listaEmprestimentos = new ArrayList<>();
 
     // Função para cadastrar funcionario
-    public void cadastrarFuncionario(){
-        scanner.nextLine();
-        System.out.println("--- Cadastrar novo funcionario ---");
-        System.out.println("Digite o código do funcionario: ");
-        String codFuncionarioTemp = scanner.nextLine();
-        System.out.println("Digite o nome do funcionário: ");
-        String nomeFuncionarioTemp = scanner.nextLine();
-        System.out.println("Digite a função do funcionáro: ");
-        String funcaoFuncionarioTemp = scanner.nextLine();
-        System.out.println("Digite a data de admissão: ");
-        String dataAdmissaoTemp = scanner.nextLine();
-
-        listaFuncionarios.add(new Funcionario(codFuncionarioTemp, nomeFuncionarioTemp, funcaoFuncionarioTemp, dataAdmissaoTemp));
+    static public void cadastrarFuncionario(Funcionario funcionario){
+        listaFuncionarios.add(funcionario);
     }
+
+    // Função para cadastrar novo equipamento
+    static public void cadastrarEquipamento(Equipamento equipamento){
+        listaEquipamentos.add(equipamento);
+    }
+
+    // Função para cadastrar novo emprestimo de equipamento
+    static public void novoEmprestimo(Emprestimos emprestimo){
+        listaEmprestimentos.add(emprestimo);
+    }
+
+
+    //Função para listar os funcionarios cadastrados
+    static public void listarFuncionariosCad(){
+        if (!listaFuncionarios.isEmpty()){
+            for (Funcionario funcTemp:listaFuncionarios){
+                funcTemp.infoFuncionario();
+            }
+        }else{
+            System.out.println("Não foi cadastrado nenhum funcionario cadastrado");
+        }
+    }
+
+    // Função para listar os equipamento cadastrados
+    static public void listarEquipamentosCad(){
+        if (!listaEquipamentos.isEmpty()){
+            for (Equipamento eqpTemp:listaEquipamentos){
+                eqpTemp.infoEquipamento();
+            }
+        }else{
+            System.out.println("Não foi cadastrado nenhum equipamento.");
+        }
+    }
+
+    //Função para listar os emprestimos cadastrados
+    static public void listarEmprestimosCad(){
+        if (!listaEmprestimentos.isEmpty()){
+            for (Emprestimos empTemp:listaEmprestimentos){
+                empTemp.infoEmprestimo();
+            }
+        }else{
+            System.out.println("Não foi cadastrado nenhum emprestimo.");
+        }
+    }
+
+
+    //Função para buscar funcionario pelo codFuncionario
+    static public Funcionario buscarFuncionario(String codFuncionario){
+       if (!listaFuncionarios.isEmpty()){
+           for(Funcionario funcTemp:listaFuncionarios){
+               if (funcTemp.getCodFuncionario().equals(codFuncionario)){
+                   return funcTemp;
+               }
+           }
+       }else{
+           System.out.println("Não foi cadastrado nenhum funcionario cadastrado");
+       }
+       return null;
+    }
+
+
+    //Função para buscar equipamento
+    static public Equipamento buscarEquipamento(String codBusca){
+        if (!listaEquipamentos.isEmpty()){
+            for (Equipamento eqpTemp:listaEquipamentos){
+                if (eqpTemp.getCodEquipamento().equals(codBusca)){
+                    return eqpTemp;
+                }
+            }
+        }else{
+            System.out.println("Não foi cadastrado nenhum equipamento.");
+        }
+        return null;
+    }
+
+
+    // Função para exibir informações do menu;
+    static public void menu(){
+        System.out.println("--- MENU ---");
+        System.out.println("[1] - Cadastrar novo funcionário");
+        System.out.println("[2] - Cadastrar novo equipamento");
+        System.out.println("[3] - Cadastrar novo emprestimo");
+        System.out.println("[4] - Listar funcionários cadastrados");
+        System.out.println("[5] - Listar equipamentos cadastrados");
+        System.out.println("[6] - Listar emprestimos de equipamentos");
+        System.out.println("[7] - Buscar funcionario pelo código");
+        System.out.println("[8] - Buscar equipamento pelo código");
+        System.out.println("Digite a opção desejada:");
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,98 @@ public class Main {
         int sair = 0;
         int opcao;
         do {
-            main.menu();
+            GerenciadorTrackBug.menu();
             opcao = main.scanner.nextInt();
             switch (opcao){
                 case 1:
-                    GerenciadorTrackBug.cadastrarFuncionario();
+                    main.scanner.nextLine();
+
+                    System.out.println("--- Cadastrar novo funcionario ---");
+                    System.out.println("Digite o código do funcionario: ");
+                    String codFuncionarioTemp = main.scanner.nextLine();
+                    System.out.println("Digite o nome do funcionário: ");
+                    String nomeFuncionarioTemp = main.scanner.nextLine();
+                    System.out.println("Digite a função do funcionáro: ");
+                    String funcaoFuncionarioTemp = main.scanner.nextLine();
+                    System.out.println("Digite a data de admissão: ");
+                    String dataAdmissaoTemp = main.scanner.nextLine();
+
+                    Funcionario funcionario = new Funcionario(codFuncionarioTemp, nomeFuncionarioTemp, funcaoFuncionarioTemp, dataAdmissaoTemp);
+                    GerenciadorTrackBug.cadastrarFuncionario(funcionario);
+
                     break;
                 case 2:
-                    main.cadastrarEquipamento();
+                    main.scanner.nextLine();
+                    System.out.println("--- Cadastrar equipamento ---");
+                    System.out.println("Digite o código do equipamento: ");
+                    String codEqpTemp = main.scanner.nextLine();
+                    System.out.println("Digite a descrição do equipamento: ");
+                    String descricaoEqpTemp = main.scanner.nextLine();
+                    System.out.println("Digite a data da compra: ");
+                    String dataCompraTemp = main.scanner.nextLine();
+                    System.out.println("Digite o peso do equipamento: ");
+                    double pesoEqpTemp = main.scanner.nextDouble();
+                    System.out.println("Digite a  largura do equipamento: ");
+                    double larguraEqpTemp = main.scanner.nextDouble();
+                    System.out.println("Digite o comprimento do equipamento: ");
+                    double comprimentoEqpTemp = main.scanner.nextDouble();
+                    main.scanner.nextLine();
+                    System.out.println("Digite o histórico de manutenção do equipamento: ");
+                    String historicoManutencaoTemp = main.scanner.nextLine();
+                    System.out.println("Digite o estado de conservação do equipamento: ");
+                    String estadoConsTemp = main.scanner.nextLine();
+
+                    Equipamento equipamento = new Equipamento(codEqpTemp, descricaoEqpTemp, dataCompraTemp, pesoEqpTemp, larguraEqpTemp, comprimentoEqpTemp, historicoManutencaoTemp, estadoConsTemp);
+                    GerenciadorTrackBug.cadastrarEquipamento(equipamento);
                     break;
                 case 3:
-                    main.novoEmprestimo();
+                    main.scanner.nextLine();
+                    System.out.println("--- Novo emprestimo ---");
+                    System.out.println("Digite a data e hora de saída: ");
+                    String dataSaidaTemp = main.scanner.nextLine();
+                    System.out.println("Digite a data e hora de retorno: ");
+                    String dataRetornoTemp = main.scanner.nextLine();
+                    System.out.println("Digite o nome do funcionario responsavel: ");
+                    String funcionarioRespTemp = main.scanner.nextLine();
+                    System.out.println("Observações do emprestimo: ");
+                    String obsEmprestimoTemp = main.scanner.nextLine();
+
+                    Emprestimos emprestimo = new Emprestimos(dataSaidaTemp, dataRetornoTemp, funcionarioRespTemp, obsEmprestimoTemp);
+                    GerenciadorTrackBug.novoEmprestimo(emprestimo);
+
                     break;
                 case 4:
-                    main.listarFuncionariosCad();
+                    GerenciadorTrackBug.listarFuncionariosCad();
                     break;
                 case 5:
-                    main.listarEquipamentosCad();
+                    GerenciadorTrackBug.listarEquipamentosCad();
                     break;
                 case 6:
-                    main.listarEmprestimosCad();
+                    GerenciadorTrackBug.listarEmprestimosCad();
                     break;
                 case 7:
-                    main.buscarFuncionario();
+                    main.scanner.nextLine(); // Limpar buffer do input
+                    System.out.println("Digite o codigo do funcionario que deseja buscar: ");
+                    String codBuscaFunc = main.scanner.nextLine(); // Variavel que será buscada
+                    Funcionario funcionarioEncontrado = GerenciadorTrackBug.buscarFuncionario(codBuscaFunc);
+
+                    if (funcionarioEncontrado != null) {
+                        funcionarioEncontrado.infoFuncionario();
+                    } else {
+                        System.out.println("Funcionario não encontrado.");
+                    }
                     break;
                 case 8:
-                    main.buscarEquipamento();
+                    main.scanner.nextLine(); // Limpar buffer do input
+                    System.out.println("Digite o codigo do Equipamento que deseja buscar: ");
+                    String codBuscaEqp = main.scanner.nextLine(); // Variavel que será buscada
+
+                    Equipamento equipamentoEncontrado = GerenciadorTrackBug.buscarEquipamento(codBuscaEqp);
+                    if (equipamentoEncontrado != null) {
+                        equipamentoEncontrado.infoEquipamento();
+                    } else {
+                        System.out.println("Equipamento não encontrado.");
+                    }
                     break;
             }
             System.out.println("Deseja sair do sistema? [0]Não [1]Sim: ");
@@ -44,146 +110,4 @@ public class Main {
         main.scanner.close();
     }
 
-    // Função para cadastrar novo equipamento
-    public void cadastrarEquipamento(){
-        scanner.nextLine();
-        System.out.println("--- Cadastrar equipamento ---");
-        System.out.println("Digite o código do equipamento: ");
-        String codEqpTemp = scanner.nextLine();
-        System.out.println("Digite a descrição do equipamento: ");
-        String descricaoEqpTemp = scanner.nextLine();
-        System.out.println("Digite a data da compra: ");
-        String dataCompraTemp = scanner.nextLine();
-        System.out.println("Digite o peso do equipamento: ");
-        double pesoEqpTemp = scanner.nextDouble();
-        System.out.println("Digite a  largura do equipamento: ");
-        double larguraEqpTemp = scanner.nextDouble();
-        System.out.println("Digite o comprimento do equipamento: ");
-        double comprimentoEqpTemp = scanner.nextDouble();
-        scanner.nextLine();
-        System.out.println("Digite o histórico de manutenção do equipamento: ");
-        String historicoManutencaoTemp = scanner.nextLine();
-        System.out.println("Digite o estado de conservação do equipamento: ");
-        String estadoConsTemp = scanner.nextLine();
-
-        listaEquipamentos.add(new Equipamento(codEqpTemp, descricaoEqpTemp, dataCompraTemp, pesoEqpTemp, larguraEqpTemp, comprimentoEqpTemp, historicoManutencaoTemp, estadoConsTemp));
-
-    }
-
-    // Função para cadastrar novo emprestimo de equipamento
-    public void novoEmprestimo(){
-        scanner.nextLine();
-        System.out.println("--- Novo emprestimo ---");
-        System.out.println("Digite a data e hora de saída: ");
-        String dataSaidaTemp = scanner.nextLine();
-        System.out.println("Digite a data e hora de retorno: ");
-        String dataRetornoTemp = scanner.nextLine();
-        System.out.println("Digite o nome do funcionario responsavel: ");
-        String funcionarioRespTemp = scanner.nextLine();
-        System.out.println("Observações do emprestimo: ");
-        String obsEmprestimoTemp = scanner.nextLine();
-
-        listaEmprestimentos.add(new Emprestimos(dataSaidaTemp, dataRetornoTemp, funcionarioRespTemp, obsEmprestimoTemp));
-    }
-
-    //Função para listar os funcionarios cadastrados
-    public void listarFuncionariosCad(){
-        int indexFinalFunc = listaFuncionarios.size() - 1;
-
-        if (indexFinalFunc==-1){
-            System.out.println("Não foi cadastrado nenhum funcionário.");
-        }else{
-            for(int i=0; i<=indexFinalFunc; i++){
-                listaFuncionarios.get(i).infoFuncionario();
-            }
-        }
-    }
-
-    // Função para listar os equipamento cadastrados
-    public void listarEquipamentosCad(){
-        int indexFinalEqp = listaEquipamentos.size() - 1;
-
-        if (indexFinalEqp<0){
-            System.out.println("Não foi cadastrado nenhum equipamento.");
-        }else{
-            for(int i=0; i<=indexFinalEqp; i++){
-                listaEquipamentos.get(i).infoEquipamento();
-            }
-        }
-    }
-
-    //Função para listar os emprestimos cadastrados
-    public void listarEmprestimosCad(){
-        int indexFinalEmp = listaEmprestimentos.size() - 1;
-
-        if (indexFinalEmp<0){
-            System.out.println("Não foi cadastrado nenhum emprestimo.");
-        }else{
-            for(int i=0; i<=indexFinalEmp; i++){
-                listaEmprestimentos.get(i).infoEmprestimo();
-            }
-        }
-    }
-
-    //Função para buscar funcionario pelo codFuncionario
-    public void buscarFuncionario(){
-        scanner.nextLine(); // Limpar buffer do input
-        System.out.println("Digite o codigo do funcionario que deseja buscar: ");
-        String codBusca = scanner.nextLine(); // Variavel que será buscada
-
-        int posicao = -1; // Posição inicial se o vetor está vazio
-
-        for (int i=0; i<listaFuncionarios.size(); i++){ // For para percorrer a lista de elementos
-            Funcionario funcionarioBusca = listaFuncionarios.get(i); // Objeto temporario usado para comparar os elementos
-
-            if (funcionarioBusca.getCodFuncionario().equals(codBusca)){ // Valida se o cod do objeto do vetor é igual ao digitado
-                posicao=i; // Se for verdadeiro a variavel "posição" armazena o indice encontrado
-                break;
-            }
-        }
-
-        if (posicao != -1){ // Se "posição" não for "-1" o objeto foi encontrado
-            listaFuncionarios.get(posicao).infoFuncionario(); // Exibir as informações do equipamento no indice
-        }else{ // caso "posição" seja "-1" entrará no Else
-            System.out.println("Funcionario não encontrado.");
-        }
-    }
-
-    //Função para buscar equipamento
-    public void buscarEquipamento(){
-        scanner.nextLine(); // Limpar buffer do input
-        System.out.println("Digite o codigo do Equipamento que deseja buscar: ");
-        String codBusca = scanner.nextLine(); // Variavel que será buscada
-
-        int posicao = -1; // Posição inicial se o vetor está vazio
-
-        for (int i=0; i<listaFuncionarios.size(); i++){ // For para percorrer a lista de elementos
-            Funcionario funcionarioBusca = listaFuncionarios.get(i); // Objeto temporario usado para comparar os elementos
-
-            if (funcionarioBusca.getCodFuncionario().equals(codBusca)){ // Valida se o cod do objeto do vetor é igual ao digitado
-                posicao=i; // Se for verdadeiro a variavel "posição" armazena o indice encontrado
-                break;
-            }
-        }
-
-        if (posicao != -1){ // Se "posição" não for "-1" o objeto foi encontrado
-            listaFuncionarios.get(posicao).infoFuncionario(); // Exibir as informações do equipamento no indice
-        }else{ // caso "posição" seja "-1" entrará no Else
-            System.out.println("Equipamento não encontrado.");
-        }
-    }
-
-    // Função para exibir informações do menu;
-    public void menu(){
-        System.out.println("--- MENU ---");
-        System.out.println("[1] - Cadastrar novo funcionário");
-        System.out.println("[2] - Cadastrar novo equipamento");
-        System.out.println("[3] - Cadastrar novo emprestimo");
-        System.out.println("[4] - Listar funcionários cadastrados");
-        System.out.println("[5] - Listar equipamentos cadastrados");
-        System.out.println("[6] - Listar emprestimos de equipamentos");
-        System.out.println("[7] - Buscar funcionario pelo código");
-        System.out.println("[8] - Buscar equipamento pelo código");
-        System.out.println("Digite a opção desejada:");
-    }
 }
